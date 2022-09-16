0355a5289234bc8bd08666e8337d3f3ed7d702da
Aravind R <ndarvi48332@gmail.com>
2022-09-16 14:36:15 +0530

    upadations and image croping

--- bookworld/bookworld/settings.py
+++ bookworld/bookworld/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 from decouple import config
+from easy_thumbnails.conf import Settings as thumbnail_settings
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -54,6 +55,8 @@ INSTALLED_APPS = [
     'orders',
     'salesreport',
     'notfoundhandler',
+    'easy_thumbnails',
+    'image_cropping',
     
 ]
 
@@ -142,6 +145,15 @@ MEDIA_URL = '/media/'
 # MEDIA_ROOT = BASE_DIR / 'media' 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
+
+
+THUMBNAIL_PROCESSORS = (
+    'image_cropping.thumbnail_processors.crop_corners',
+) + thumbnail_settings.THUMBNAIL_PROCESSORS
+
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+
+
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
