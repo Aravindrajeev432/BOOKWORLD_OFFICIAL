13ef21c0dc18405931d9a801940db7a6db39f399
Aravind R <ndarvi48332@gmail.com>
2022-09-19 11:56:51 +0530

    s3

--- bookworld/bookworld/settings.py
+++ bookworld/bookworld/settings.py
@@ -153,7 +153,7 @@ AWS_DEFAULT_ACL = 'public-read'
 AWS_LOCATION = 'static'
 
 STATICFILES_DIRS = [
-    'bookworld/static',
+    'static',
 ]
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
