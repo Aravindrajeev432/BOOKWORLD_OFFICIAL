3c9fca7c41cb03a97c89ab46e966eaefc1102f95
Aravind R <ndarvi48332@gmail.com>
2022-09-19 11:18:59 +0530

    s3

--- bookworld/bookworld/settings.py
+++ bookworld/bookworld/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'orders',
     'salesreport',
     'notfoundhandler',
+    'storages',
     
 ]
 
@@ -135,9 +136,32 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
-STATIC_ROOT  = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
+# STATIC_ROOT  = os.path.join(BASE_DIR, 'staticfiles')
+
+# AWS S3 Static Files Configuration
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = 'public-read'
+AWS_LOCATION = 'static'
+
+STATICFILES_DIRS = [
+    'greatkart/static',
+]
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+DEFAULT_FILE_STORAGE = 'greatkart.media_storages.MediaStorage'
+
+
+
 MEDIA_URL = '/media/'
 # MEDIA_ROOT = BASE_DIR / 'media' 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
