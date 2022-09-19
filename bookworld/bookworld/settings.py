1d6943a658550f286cd38ba0d99aaf489f6313ca
Aravind R <ndarvi48332@gmail.com>
2022-09-19 12:38:04 +0530

    media s3

--- bookworld/bookworld/settings.py
+++ bookworld/bookworld/settings.py
@@ -159,7 +159,7 @@ STATICFILES_DIRS = [
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-
+DEFAULT_FILE_STORAGE = 'bookworld.media_storages.MediaStorage'
 
 
 
